cfb363145770d392a624edffd69c175caf79858d
Brent Scarafone <brentscarafone@gmail.com>
2022-03-31 03:10:05 -0600

    Added in is-empty package, gitignore

---
+++ is-empty/is-empty.js
@@ -0,0 +1,19 @@
+/**
+ * Helper util to qualify response data as equivalent to "null"
+ * Object could be empty
+ * Object could be undefined
+ * Object could null
+ * All will result in a true value, otherwise false
+ */
+
+
+module.exports = function IsEmpty(obj) {
+    if (!obj) return true
+    if (obj === undefined) return true
+    if (obj && obj.length <= 0) return true
+    if(typeof obj === 'object') {
+        const isEmpty = Object.keys(obj).length <= 0
+        return isEmpty
+    }
+    return false
+}
