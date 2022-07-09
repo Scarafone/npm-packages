8562e129339bd624bf27a3dbe4080f3fcba4a357
Brent Scarafone <brentscarafone@gmail.com>
2022-07-08 19:58:35 -0600

    Updated packages

---
+++ pkgs/clinjs/index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+
+
+console.log("Yet Another Template Engine");
+
+const yargs = require("yargs")
+const { hideBin } = require("yargs/helpers")
+const argv = yargs(hideBin(process.argv)).argv
+
+    
