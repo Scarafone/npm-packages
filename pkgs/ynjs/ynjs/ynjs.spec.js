576b6685b5f57cd666aaca983e5c0cc84c2cbace
Brent Scarafone <brentscarafone@gmail.com>
2022-08-02 00:39:35 -0600

    WIP: Updating ynjs spec

--- pkgs/ynjs/ynjs/ynjs.spec.js
+++ pkgs/ynjs/ynjs/ynjs.spec.js
@@ -1,9 +1,14 @@
 const ynjs = require('./index')
+const { dele } = require('@scarafone/files-helper')
 
 describe('YNJS Test Suite', () => {
 
     it('should import package as function', () => {
         expect(typeof ynjs).toBe('object')
+    })
+
+    it('should execute init command', () => {
+
     })
 
 })
